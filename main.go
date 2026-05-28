f4bd35daef2f11fa1a1233dfb5af47caa8029f3b
MoltenWolfCub <oliver@tomlins.org.uk>
2026-05-28 22:40:15 +0100

    implemented editing items

--- main.go
+++ main.go
@@ -67,6 +67,8 @@ func main() {
 	http.HandleFunc("/wishlist/", handleWishlist)
 	http.HandleFunc("/wishlist/add_item", handleAddWishlistItem)
 	http.HandleFunc("POST /wishlist/add_item/submit", handleAddWishlistItemSubmit)
+	http.HandleFunc("/wishlist/edit_item", handleEditWishlistItem)
+	http.HandleFunc("POST /wishlist/edit_item/submit", handleEditWishlistItemSubmit)
 	http.HandleFunc("POST /wishlist/delete_item", handleDeleteWishlistItem)
 	log.Fatal(http.ListenAndServe(":8040", nil))
 }
@@ -109,6 +111,36 @@ func handleAddWishlistItemSubmit(w http.ResponseWriter, r *http.Request) {
 	name := r.FormValue("itemName")
 
 	db.Exec("INSERT INTO wishlist_items (wishlist_id, item_name) VALUES (1, ?);", name)
+
+	http.Redirect(w, r, "/wishlist", http.StatusFound)
+}
+
+func handleEditWishlistItem(w http.ResponseWriter, r *http.Request) {
+	editID := r.FormValue("editID")
+	var item WishlistItem
+	var _nil0 string
+	err := db.QueryRow("SELECT * FROM wishlist_items WHERE id=?", editID).Scan(&item.Id, &_nil0, &item.Name)
+	if err != nil {
+		log.Print("Error finding item to edit:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	t, err := template.ParseFiles(
+		filepath.Join(templatesPath, "wishlist_edit.html"),
+	)
+	if err != nil {
+		log.Print("Error parsing template:", err)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+
+	t.Execute(w, item)
+}
+
+func handleEditWishlistItemSubmit(w http.ResponseWriter, r *http.Request) {
+	name := r.FormValue("itemName")
+	editID := r.FormValue("editID")
+
+	db.Exec("UPDATE wishlist_items SET item_name=? WHERE id=?", name, editID)
 
 	http.Redirect(w, r, "/wishlist", http.StatusFound)
 }
