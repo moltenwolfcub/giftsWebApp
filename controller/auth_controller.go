42da4eed4814fb73caec83e9d6b6f3d35ebf9387
MoltenWolfCub <oliver@tomlins.org.uk>
2026-05-29 23:19:51 +0100

    applied helper func to auth_controller

--- controller/auth_controller.go
+++ controller/auth_controller.go
@@ -2,10 +2,7 @@ package controller
 
 import (
 	"database/sql"
-	"log"
 	"net/http"
-	"path/filepath"
-	"text/template"
 )
 
 type authController struct {
@@ -13,16 +10,7 @@ type authController struct {
 }
 
 func (c *authController) register(w http.ResponseWriter, r *http.Request) {
-	t, err := template.ParseFiles(
-		filepath.Join(templatesPath, "_base.html"),
-		filepath.Join(templatesPath, "auth_register.html"),
-	)
-	if err != nil {
-		log.Print("Error parsing template:", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
-
-	t.Execute(w, nil)
+	serveTemplate(w, "auth_register.html", nil)
 }
 
 func (c *authController) registerSubmit(w http.ResponseWriter, r *http.Request) {
