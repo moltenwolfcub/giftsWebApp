ac9008fe045d9e3b1b35c1f3a70ac86df4ee0dca
MoltenWolfCub <oliver@tomlins.org.uk>
2026-05-30 22:50:08 +0100

    started framework for testing

---
+++ controller/wishlist_controller_test.go
@@ -0,0 +1,37 @@
+package controller_test
+
+import (
+	"database/sql"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/moltenwolfcub/giftsWebApp/config"
+	"github.com/moltenwolfcub/giftsWebApp/controller"
+)
+
+func genDataBase() (*sql.DB, func()) {
+	return nil, nil
+}
+
+func TestAddItem(t *testing.T) {
+	db, teardownDB := genDataBase()
+	defer teardownDB()
+	cfg := config.New()
+	router := controller.BuildRouter(db, cfg)
+
+	req := httptest.NewRequest("POST", "/wishlist/add_item", nil)
+	w := httptest.NewRecorder()
+
+	router.ServeHTTP(w, req)
+
+	res := w.Result()
+	defer res.Body.Close()
+
+	if res.StatusCode != http.StatusFound { //TODO check all status codes used are correct
+		t.Errorf("Wrong http status code: %d", res.StatusCode)
+	}
+
+	// db.QueryRow()
+	// check row correctly added to wishlist_items
+}
