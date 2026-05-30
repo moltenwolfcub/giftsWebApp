83bfb6b596391821992cedca613d793b48cfde54
MoltenWolfCub <oliver@tomlins.org.uk>
2026-05-30 20:24:27 +0100

    updated dev migration to put a proper dev user in

--- database/migrations/_dev_populate.sql
+++ database/migrations/_dev_populate.sql
@@ -1,4 +1,10 @@
-INSERT INTO users (username, password, salt) VALUES ("dev", "tmpPass", "tmpSalt");
+INSERT INTO users (username, password, salt) VALUES (
+	"dev",
+	"nil",
+	"devSalt"
+);
+UPDATE users SET password=X'3a4d722d5f2f1908bbc96eeeafcacebb4cc53187698f75d07de9fd144adc9681'
+WHERE username="dev";
 
 INSERT INTO wishlists (user_id) VALUES (1);
 INSERT INTO wishlist_items (wishlist_id, item_name) VALUES (1, "game");
